3dc773c80a068842b14a58bd767ad0b214d4065a
sidhrashid <sidhpurarashid298@gmail.com>
2026-01-05 02:16:23 +0530

    hero sectin modify

--- src/pages/client/home/Hero.jsx
+++ src/pages/client/home/Hero.jsx
@@ -13,24 +13,24 @@ const Hero = () => {
       />
 
       {/* Content Overlay - Positioned on the image */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center md:items-start">
-        <div className="max-w-7xl mx-auto px-6 sm:px-6 lg:px-8 w-full md:pt-28 lg:pt-36">
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex items-start">
+        <div className="max-w-7xl mx-auto px-6 sm:px-6 lg:px-8 w-full pt-16 md:pt-28 lg:pt-36">
           <div className="max-w-md lg:max-w-lg bg-transparent p-4 md:p-0">
             {/* Main Heading - Line 1 */}
-            <h1 className="text-xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-gray-800 leading-tight">
+            <h1 className="text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-gray-800 leading-tight">
               Exporters of Premium
             </h1>
 
             {/* Sub Heading - Line 2 */}
-            <h2 className="text-base sm:text-lg md:text-lg lg:text-xl xl:text-2xl font-semibold text-gray-700 mb-3 sm:mb-2 ">
-              Indian Spices & Food Products{" "}
-              <span className="text-sm sm:text-xs md:text-sm font-normal">to the</span>{" "}
+            <h2 className="text-lg sm:text-lg md:text-lg lg:text-xl xl:text-2xl font-semibold text-gray-700 mb-3 sm:mb-2 ">
+              Indian Spices & Food Products  <br />
+              <span className="text-sm sm:text-sm md:text-md font-normal">to the</span>{" "}
               <span className="text-xl sm:text-lg md:text-xl lg:text-2xl font-bold">World</span>
             </h2>
             <div className="border-b border-gray-400 w-3/4 mb-3"></div>
             
-            {/* Description with underline */}
-            <div className="mb-5 sm:mb-4 md:mb-5">
+            {/* Description with underline - Hidden on mobile */}
+            <div className="hidden md:block mb-5 sm:mb-4 md:mb-5">
               <p className="text-xs sm:text-[10px] md:text-xs text-gray-600 pb-1 sm:pb-2 border-b border-gray-400 inline-block font-medium">
                 Manufactured by Abaad Masala & Co. (india) || Exported by Nexus Global Oversaes
               </p>
