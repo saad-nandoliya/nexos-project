adfd5a23959de9394b8e9d053ca23c7e797fcb8d
Saad Nandoliya <saadnandoliya99@gmail.com>
2026-01-06 15:15:25 +0530

    feat: Add Hero section component to the client home page with marketing content and a CTA.

--- src/pages/client/home/Hero.jsx
+++ src/pages/client/home/Hero.jsx
@@ -14,17 +14,17 @@ const Hero = () => {
       />
 
       {/* Content Overlay */}
-      <div className="absolute inset-0 flex md:items-start lg:left-40 md:left-0 md:bottom-28 top-20 left-2 sm:top-0  ">
+      <div className="absolute inset-0 flex md:items-start 2xl:top-52 lg:left-40  md:bottom-28 top-20 left-2 sm:top-0  ">
         <div className="max-w-7xl mx-auto px-5 sm:px-6 lg:px-8 w-full pt-12 sm:pt-14 md:pt-24 lg:pt-36">
           <div className="max-w-md lg:max-w-lg">
 
             {/* Heading */}
-            <h1 className="text-md md:text-xl lg:text-2xl xl:text-3xl font-bold text-gray-800 leading-tight">
+            <h1 className="text-md md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-5xl font-bold text-gray-800 leading-tight">
               Exporters of Premium
             </h1>
 
             {/* Sub Heading */}
-            <h2 className="text-xs md:text-sm lg:text-md xl:text-lg font-bold text-gray-700 mt-1 mb-2">
+            <h2 className="text-xs md:text-sm lg:text-md xl:text-lg 2xl:text-2xl 3xl:text-3xl font-bold text-gray-700 mt-1 mb-2">
               Indian Spices & Food Products {" "}
               <span className="text-xs md:text-sm lg:text-sm font-normal">to the</span>{" "}
               <span className="text-xs md:text-sm lg:text-md xl:text-lg  font-bold">
@@ -35,11 +35,11 @@ const Hero = () => {
 
 <div className="mb-7">
             {/* Divider */}
-            <div className="border-b border-gray-400 md:w-3/4 w-2/4 "></div>
-              <p className="text-[5px] md:text-[9.7px] text-gray-600 pb-1  inline-block font-medium tracking-wide">
+            <div className="border-b border-gray-400 md:w-3/4 w-2/4 2xl:w-full "></div>
+              <p className="text-[5px] md:text-[9.7px]  2xl:text-[12.8px] 3xl:text-[14px] text-gray-600 pb-1  inline-block font-medium tracking-wide">
                 Manufactured by Abaad Masala & Co. (India) || Exported by Nexus Global Overseas
               </p>
-            <div className="border-b border-gray-400 md:w-3/4 w-2/4 mb-2"></div>
+            <div className="border-b border-gray-400 md:w-3/4 w-2/4 2xl:w-full mb-2"></div>
 
 
 </div>
